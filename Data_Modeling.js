6ffef0d0a0e48a75ac729c9dac1a55bbfdabe4ca
nadaa <nada.saif7@gmail.com>
2018-02-12 10:00:21 +0200

    solve data modeling toy problem'

--- Data_Modeling.js
+++ Data_Modeling.js
@@ -7,3 +7,79 @@
 	// 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
 	// 	-Write a function searchMates that, given a query and an array of Mates,
 	//           searche the array of mates for "matching" mate. You will decide what way you want to write in your search algorithm.
+	//           
+	//           
+	//      
+	//           
+	//                
+	
+
+
+function createClassMate(name,age,gender,nationality){
+    return {
+        name:name,
+        age:age,
+        gender:gender,
+        nationality:nationality
+    }
+	}
+
+
+	function dispalyFriend(mate){
+
+    return 'Name: '+mate.name+'Age: '+mate.age+'Gender: '+mate.gender+'Nationality: '+mate.nationality;
+	}
+
+
+
+	function addFriend(mate){
+    mates.push(mate);
+
+	}
+
+
+
+	function nbOfMale(){
+    var maleCount=0;
+
+    for(var i=0;i<mates.length;i++){
+      if (mates[i].gender==='male')
+      {
+      	maleCount++;}
+
+    }
+	
+	return maleCount;
+}
+
+
+
+
+
+function searchMate(mates,name){
+for (var i=0;i<mates.length;i++){
+  if (mates[i].name===name){
+
+  	return true;
+  }
+
+}
+return false;
+
+}
+
+	mate1=createClassMate('Waed',26,'female','Jordenian'};
+	mate2=createClassMate('Bayan',20,'female','Serian'};
+	mate3=createClassMate('salsabeel',26,'female','Jordenian'};
+
+    mate4=createClassMate('Belal',25,'male','Jordenian')
+
+	var mates=[mate1,mate2,mate3,mate4];
+
+
+
+
+
+
+
+	
