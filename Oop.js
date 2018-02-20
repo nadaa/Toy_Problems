940014939be397b5ec7e0051f13066717713a141
nadaa <nada.saif7@gmail.com>
2018-02-20 09:24:26 +0200

    toy of OOP1

--- Oop.js
+++ Oop.js
@@ -2,29 +2,69 @@
 // Refactor the code as an MakeGame class that shares its methods across different instances.
 
 
+
+
+
+
 function randInt(n) {
     return Math.floor(Math.random() * (n + 1));
 }
 
-function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
+
+
+var guessMyNumber=function(n){
+  		this.counter++;
+	    if (n > this.upper) {
+	      return "Out of bounds! Please try a number between 0 and " + this.upper + ".";
+	    } else if (n === this.rand) {
 	      return "You guessed my number!";
 	    }
 	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
+  	}
+
+
+var giveUp=function(){
     	return rand;
-  	},
-  	numOfGuesses:function(){
+  	}
+
+var numOfGuesses=function(){
     	return counter;
   	}
-  }
+
+
+function makeGame(upperbound){
+	var game={};
+	game.upper=upperbound;
+	game.rand=randInt(upperbound);
+	game.counter=0;
+	game.guessMyNumber=guessMyNumber;
+	game.giveUp=giveUp;
+	game.numOfGuesses=numOfGuesses;
+
+	return game;
 }
+
+
+
+// function makeGame(upperbound){
+//   var upper=upperbound;
+//   var rand=randInt(upper)
+//   var counter=0;
+//   return {
+//   	guessMyNumber:function(n){
+//   		counter++;
+// 	    if (n > upper) {
+// 	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+// 	    } else if (n === rand) {
+// 	      return "You guessed my number!";
+// 	    }
+// 	    return "Nope! That wasn't it!";
+//   	},
+//   	giveUp:function(){
+//     	return rand;
+//   	},
+//   	numOfGuesses:function(){
+//     	return counter;
+//   	}
+//   }
+// }
