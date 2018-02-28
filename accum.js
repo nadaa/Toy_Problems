b2414640a8f99e149bf1df44c34d1e5980613dd7
nadaa <nada.saif7@gmail.com>
2018-02-28 09:39:06 +0200

    swap string'

--- accum.js
+++ accum.js
@@ -5,5 +5,48 @@
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
     function accum(input){
-        //your code is here
-    }
+       // repeat each char in a given string number of times equal to its index, convert only the first charector to capital.
+       
+
+       //1. start by iterating over charecters
+       //2. user "repeat" function to repeat each charecter number of its index
+       // define variable 'result' to store the result
+       
+
+      
+        //var inputArr=input.split('');
+       //var result=input[0].toUpperCase()+'-';
+       var result=''
+       for (var i=0;i<input.length;i++){
+
+	       	  var strIn=input[i].repeat(i+1).toLowerCase();
+	       	  //console.log(strIn)
+	       	  strIn=strIn[0].toUpperCase()+strIn.slice(1);
+	       	  result+=strIn+'-';
+	          strIn='';
+      }
+      result=result.slice(0,result.length-1);  
+      return result;
+       
+       }
+
+
+
+
+
+    // optimized sol using higher order fun
+    
+
+    // // convert the string into an array
+    // // apply reduce function
+    
+    // var inputArr=input.split('');
+    // inputArr.reduce(function(acc,elem){
+    // 	elem=elem.toLowerCase();
+    // 	return acc+elem[0].toUpperCase()+elem.slice(1).repeat()+'-';
+
+    // },'')
+
+
+
+
--- swap.js
+++ swap.js
@@ -1,6 +1,29 @@
 	// Given a string, swap the case for each of the letters.
 	// IbrAHiM --> iBRahIm
 	// ToYPRoblEm --> tOyprOBLeM
+	// 
+	// 
+	// 
+	
+
 	function swap(input){
-		//your code is here
+		// since string is immutable, we can convert it first to an array
+		strArr=input.split('');
+
+       // reduce function with acc='' will return the swapped string
+		return strArr.reduce(function(acc,elem){
+			
+			// ascii range for upper case [65-90]
+			if (elem.charCodeAt()>=65 && elem.charCodeAt()<=90)
+				
+				return acc+elem.toLowerCase();
+
+			// ascii for Lower Case [97-122]
+			else if(elem.charCodeAt()>=97 && elem.charCodeAt()<=122)
+				return acc+elem.toUpperCase();
+
+			// in case the string contains non-alphabetical charecters
+			else 
+				return acc+elem;
+		},'')
 	}
